f79cbf6da85470d4d839631634df181a06d0cce7
Iman Mohi <imanmohi786@gmail.com>
2020-06-19 22:31:45 +0530

    .

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ let cells = document.querySelectorAll('div.row > div');
 
 for (let i=0; i< cells.length; i++)
 {
-    cells[i].addEventListener('click', cellClicked);
+    if (i%2===0)
+    {
+        cells[i].addEventListener('click', cellClickedcross);
+    }
+    else
+    {
+        cells[i].addEventListener('click', cellClickedzero);
+    }
 }
 
-function cellClicked()
+function cellClickedcross()
 {
-    for (let i=0; i<10; i++)
-    {
-        if(i%2==0)
-        {
-            event.target.textContent = 'X';
-        } else {
-            event.target.textContent = 'O';
-        }
-    }
+    event.target.textContent = 'X';
+}
+
+function cellClickedzero()
+{
+    event.target.textContent = 'O';
 }
