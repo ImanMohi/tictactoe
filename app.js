9a5ce66e4a70c12d39277fe23cbdba745478921a
Iman Mohi <imanmohi786@gmail.com>
2020-06-20 12:23:30 +0530

    .

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+let turnCount = 0;
 
+function startGame()
+{
+    turnCount = 0;
 
+    for(let i=0; i<=9; i++)
+    {
+        clearBox(i);
+        //set refreshbtn visibility to hidden
+        document.getElementById('refreshbtn').style.visibilty = 'hidden';
+    }
+}
+
+//Reset for another game
 function clearBox(number) {
     document.getElementById('square' + number).innerText = ''; 
+}
+
+function getVal(number)
+{
+    return document.getElementById('square' + number).innerText;
 }
